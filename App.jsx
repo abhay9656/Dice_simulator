2e4638bc390c70c36d8a7850301e430ad949f31e
abhay9656 <abhay9656gupta@gmail.com>
2024-07-13 15:51:19 +0530

    Add roll dice functionality and display result in App.jsx

--- App.jsx
+++ App.jsx
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import { StyleSheet, Text, View ,Image} from 'react-native';
+import { StyleSheet, Text, View ,Image, TouchableOpacity} from 'react-native';
 
 const dice={
   1:require('./assets/dice1.png'),
@@ -16,14 +16,32 @@ export default function App() {
   const [dice1,setDice1]=useState(1);
   const [dice2,setDice2]=useState(2);
 
+  const [result,setResult]=useState(dice1+dice2);
+
+  const rolldice=()=>{
+    const randomDice1=Math.floor(Math.random()*6)+1;
+    const randomDice2=Math.floor(Math.random()*6)+1;
+
+    setDice1(randomDice1);
+    setDice2(randomDice2);
+    setResult(randomDice1+randomDice2);
+  }
+
   return (
     <View style={styles.container}>
      
         <Text style={styles.title}>Dice Simulator</Text>
-        <View > 
+        <View style={styles.diceContainer}> 
            <Image style={styles.dice} source={dice[dice1]}/>
            <Image style={styles.dice} source={dice[dice2]}/>
-      </View>
+         </View>
+
+         <TouchableOpacity onPress={rolldice}>
+            <Text style={styles.rollButton}>Roll Dice</Text>
+         </TouchableOpacity>
+
+
+          <Text style={styles.result}>Result: {result}</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -40,5 +58,24 @@ const styles = StyleSheet.create({
     fontSize:30,
     fontWeight:'bold',
     marginBottom:30
+  },
+  diceContainer:{
+    flexDirection:'row',
+    gap:10
+  },
+  rollButton:{
+    fontSize:20,
+    fontWeight:'bold',
+    backgroundColor:'black',
+    color:'white',
+    padding:10,
+    borderRadius:10,
+    marginTop:20
+  },
+  result:{
+    fontSize:30,
+    fontWeight:'bold',
+    marginTop:20,
+    fontFamily:'monospace'
   },
 });
